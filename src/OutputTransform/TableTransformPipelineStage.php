09139054918115d8b512484cbf4e6ee2747cf75f
alex4401 <alex@wiki.gg>
2025-04-10 18:15:21 +0200

    feat: wrap tables in a div

--- src/OutputTransform/TableTransformPipelineStage.php
+++ src/OutputTransform/TableTransformPipelineStage.php
@@ -14,7 +14,27 @@ class TableTransformPipelineStage extends ContentDOMTransformStage {
 	}
 
 	public function transformDOM( Document $dom, ParserOutput $po, ?ParserOptions $popts, array &$options ): Document {
-        // TODO: implement
+        $hasBigTable = false;
+
+        foreach ( $dom->getElementsByTagName( 'table' ) as $tableElement ) {
+            if ( !preg_match( '/\bbigtable\b/', $tableElement->getAttribute( 'class' ) ) ) {
+                continue;
+            }
+
+            $hasBigTable = true;
+
+            $wrapperElement = $dom->createElement( 'div' );
+            $wrapperElement->setAttribute( 'class', 'bigtable-wrapper' );
+            $wrapperElement->setAttribute( 'data-mw-bigtable', 'true' );
+            $tableElement->replaceWith( $wrapperElement );
+            $wrapperElement->append( $tableElement );
+        }
+
+        if ( $hasBigTable ) {
+            $po->addModuleStyles( [ 'ext.bigtable.styles' ] );
+            $po->addModules( [ 'ext.bigtable' ] );
+        }
+
         return $dom;
     }
 }

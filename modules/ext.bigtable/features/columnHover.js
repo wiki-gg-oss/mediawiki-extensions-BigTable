9a600053140d8da09ad9cbc0e41aa2dc1becee8b
alex4401 <alex@wiki.gg>
2025-12-04 19:45:23 +0100

    ColumnHover: Try to adjust for colspan across the entire table

--- modules/ext.bigtable/features/columnHover.js
+++ modules/ext.bigtable/features/columnHover.js
@@ -26,19 +26,69 @@ const updateColumnHover = mw.util.debounce(
 
         clearHoverFromBody();
 
-        const column = eventTarget.cellIndex;
-
-        let hovered = Array.prototype.map.call( tableElement.rows, row => {
-            let index = column,
-                cellElement = null;
-            while ( !cellElement && index >= 0 ) {
-                cellElement = row.cells[ index ];
-                index--;
+        // Because of colspan, we should count the column IDs on our own as cellIndex is not accurate across different
+        // rows.
+        // TODO: likely can be precached but probably not worth the effort?
+        const absoluteColumnIndices = {};
+        let currentColIndex = -1;
+        // Determine first at the event target's level to handle cases like:
+        //  a B C d
+        //  a E E d   --- event target
+        //  a B B d
+        //  a B C d
+        for ( const cellElement of eventTarget.parentNode.cells ) {
+            if ( cellElement === eventTarget ) {
+                for ( let iter = 0; iter < cellElement.colSpan; iter++ ) {
+                    absoluteColumnIndices[ ++currentColIndex ] = true;
+                }
+                break;
+            } else {
+                currentColIndex += cellElement.colSpan;
             }
-            return cellElement;
-        } );
+        }
+        // Now walk over all other rows and try to project our column IDs onto the cells. If there's any misalignment in
+        // the current colspan line, we should spread the mark. This lets us handle cases like:
+        //  a B C d
+        //  a E C d   --- event target
+        //  a B B d
+        //  a B C d
+        // as without this pass, things would look more like:
+        //  a B c d
+        //  a E c d   --- event target
+        //  a B B d
+        //  a B c d
+        for ( const rowElement of tableElement.rows ) {
+            currentColIndex = -1;
+            for ( const cellElement of rowElement.cells ) {
+                currentColIndex++;
+                for ( let iter = 0; iter < cellElement.colSpan; iter++ ) {
+                    if ( absoluteColumnIndices[ currentColIndex + iter ] ) {
+                        // Great, this cell is within our index list - let's mark its colspan range now
+                        for ( iter = 0; iter < cellElement.colSpan; iter++ ) {
+                            absoluteColumnIndices[ currentColIndex + iter ] = true;
+                        }
+                        break;
+                    }
+                }
+                currentColIndex += cellElement.colSpan - 1;
+            }
+        }
 
-        for ( const cellElement of hovered ) {
+        // Now determine the cells to update
+        const columnsToUpdate = new Set();
+        for ( const rowElement of tableElement.rows ) {
+            currentColIndex = -1;
+            for ( const cellElement of rowElement.cells ) {
+                for ( let iter = 0; iter < cellElement.colSpan; iter++ ) {
+                    if ( absoluteColumnIndices[ ++currentColIndex ] ) {
+                        columnsToUpdate.add( cellElement );
+                        // Don't break this loop or currentColIndex will desync!
+                    }
+                }
+            }
+        }
+
+        for ( const cellElement of columnsToUpdate ) {
             if ( cellElement.nodeName === 'TD' ) {
                 cellElement.classList.add( HOVER_CLASS );
             }
